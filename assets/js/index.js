76ad9901935cf6d20628f93fbe5110e950349d47
Akira Shinohara <34061817+s10akir@users.noreply.github.com>
2017-12-30 02:14:03 +0900

    [add] indexページの作成
    img01.pngは仮の画像

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,23 @@
 'use strict';
 
 
+// ○×のテーブルに色付け
+function colorTF() {
+
+    $('td').each(function(i, element) {
+        if (element.innerText === "○") {
+            $(element).addClass("true");
+        } else if (element.innerText === "×") {
+            $(element).addClass("false");
+        }
+    });
+}
+
+
+colorTF();
+
 $('#header').load('header.html');
+
+$('#about-button').on('click', function () {
+    window.location.href = 'about.html';
+});
