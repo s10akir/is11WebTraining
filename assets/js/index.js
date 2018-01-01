a693a31d0c617db8df87c12024bc2e523a87f957
Akira Shinohara <34061817+s10akir@users.noreply.github.com>
2018-01-01 15:54:26 +0900

    [add] githubPages以外で開かれた場合githubPagesに誘導するアラートを表示

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,3 +21,20 @@ colorTF();
 $('#about-button').on('click', function () {
     window.location.href = 'about.html';
 });
+
+// 教員向けアラート
+const githubPagesUrl = 'https://s10akir.github.io/is11WebTraining/';
+function alert4teacher() {
+    const message = [
+        'この課題はJSを使用して作成しました。',
+        'クライアントサイドのいくつかの制約によってローカルでは動作しない実装を含んでいます。',
+        '動作は実際のWebページでの確認をお願い致します。',
+        '',
+        'github pagesへ遷移します。'
+    ].join('\n');
+    window.alert(message);
+    window.location.href = githubPagesUrl;
+}
+if (location.href !== githubPagesUrl) {
+    alert4teacher();
+}
